2db755a02a83641ebeb11cf28a89f35f2398c091
katiehutchinson <katiehutc@gmail.com>
2025-03-29 16:42:35 -0400

    adding home img

--- hoohacks/app/(tabs)/index.tsx
+++ hoohacks/app/(tabs)/index.tsx
@@ -1,18 +1,45 @@
 // Example for ingredients.tsx
 import React from 'react';
-import { View, Text, Button } from "react-native";
+import {View, Text, Button, ImageBackground, StyleSheet} from "react-native";
 import { useRouter } from "expo-router";
 
 export default function Index() {
     const router = useRouter();
 
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text style={{ fontSize: 24, marginBottom: 20 }}>Home</Text>
-            <Button
-                title="Start"
-                onPress={() => router.push("/ingredients")}
-            />
-        </View>
+        <ImageBackground
+            source = {require('./cooking-campin-home.PNG')}
+            style = {styles.backgroundImage}
+            resizeMode = "cover"
+        >
+            <View style = {styles.container}>
+                <Text style = {styles.title}> Home</Text>
+                <Button title="Start"
+                        onPress = {() => router.push("/ingredients")}
+                        />
+            </View>
+        </ImageBackground>
     );
 }
+
+
+const styles = StyleSheet.create({
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+    },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+        color: '#ffffff',
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: { width: -1, height: 1 },
+        textShadowRadius: 10
+    }
+});
